1fc9edc406579063be905c2530772a1baeac46f0
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2022-01-25 09:55:38 +0100

    finished

--- index.js
+++ index.js
@@ -10,29 +10,26 @@ app.get("/", function(req,res){
 });
 
 app.post("/", (req,res)=>{
-    console.log(req.body.cityName);
+    const query = req.body.cityName;
+    const appID = "2ed06f3d16c548cad4bf62c9a5824dfd";
+    const units = req.body.unitName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+appID+"";
+    https.get(url, (response)  =>{
+        response.on("data", (data)=>{
+            let weatherData = JSON.parse(data)
+            let weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write('<head><meta charset="utf-8"></head>');
+        res.write("<h1>The temperature in " +query+" is "+weatherData.main.temp+" degrees celsius</h1>");
+        res.write("<h3>The weather is now "+weatherDescription+"</h3>");
+        res.write(`<img src="`+imageurl +`" alt="current weather">`)
+        res.end();
+    })
+})
 })
 
 app.listen(3000, function(){
     console.log("the server is running on port 3000.")
 });
 
-// const query = "Oslo";
-// const appID = "2ed06f3d16c548cad4bf62c9a5824dfd";
-// const unit = "metric";
-// const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+appID+"";
-// https.get(url, (response)  =>{
-//     response.on("data", (data)=>{
-//         let weatherData = JSON.parse(data)
-//         console.log(weatherData.main.temp);
-//         let weatherDescription = weatherData.weather[0].description;
-
-//         const icon = weatherData.weather[0].icon;
-//         const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-//         res.write('<head><meta charset="utf-8"></head>');
-//         res.write("<h1>The temperature in Oslo is "+weatherData.main.temp+" degrees celsius</h1>");
-//         res.write("<h3>The weather is now "+weatherDescription+"</h3>");
-//         res.write(`<img src="`+imageurl +`" alt="current weather">`)
-//         res.end();
-//     })
-// })
